196623499a75698209f0664dabcb7a20a67b4f5e
vtran <vtran2535@csu.fullerton.edu>
2022-03-08 23:01:28 -0800

    Added some list elements and search designs

--- app/src/main/java/com/example/lico/MainActivity.kt
+++ app/src/main/java/com/example/lico/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.lico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter<*>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(context = this, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.resourceTitles))
+        lv_listView.adapter = adapter
+        lv_listView
+
+
+
     }
+
 }
+
