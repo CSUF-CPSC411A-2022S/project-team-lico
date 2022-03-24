fed150de36f7d0784e719a74eb2b50635910ea58
valmartnz <47126912+valmartnz@users.noreply.github.com>
2022-03-23 20:35:12 -0700

    Started creating fragments and implementing navigation.

--- app/src/main/java/com/example/lico/MainActivity.kt
+++ app/src/main/java/com/example/lico/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.lico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var clearButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val username: EditText = findViewById(R.id.username)
+        val password: EditText = findViewById(R.id.password)
+
+        val loginButton: Button = findViewById(R.id.login)
+        clearButton = findViewById(R.id.clear)
+        loginButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Adding ${username.text} @ ${password.text}.", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        // clear username and password when clear button pressed
+        clearButton.setOnClickListener {
+            username.setText("")
+            password.setText("")
+        }
     }
 }
