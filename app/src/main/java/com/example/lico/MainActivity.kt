84b432ce87482f53bce73631b869f08235dfabbf
vtran <vtran2535@csu.fullerton.edu>
2022-03-24 22:31:04 -0700

    Attempted to add an indicator to scroll through resources,and fixed some bugs

--- app/src/main/java/com/example/lico/MainActivity.kt
+++ app/src/main/java/com/example/lico/MainActivity.kt
@@ -2,22 +2,35 @@ package com.example.lico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var adapter: ArrayAdapter<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(context = this, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.resourceTitles))
-        lv_listView.adapter = adapter
-        lv_listView
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // need to add fragment into activity main assign ID to android.navhost...enables navigation(findNavController)
+        binding.button.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_nav_host_to_homePage2)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item:MenuItem): Boolean{
+        when(item.itemId){
+            R.id.nav_favorite -> Toast.makeText(this,"Favorite Selected",Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
