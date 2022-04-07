0de131b293d56462f6971eaf3a35eff8c9616db9
valmartnz <47126912+valmartnz@users.noreply.github.com>
2022-04-07 16:40:51 -0700

    Added comments to explain database and how retrieve data for right now.

--- app/src/main/java/com/example/lico/MainActivity.kt
+++ app/src/main/java/com/example/lico/MainActivity.kt
@@ -24,4 +24,29 @@ class MainActivity : AppCompatActivity() {
          NavigationUI.setupActionBarWithNavController(this,navController)
     }
 
+    // create a database (the hardest part according to prof)
+    // create multiple tables for the same database
+    // resource table, etc
+    // for each class needs its own entity
+    // if we have a view that needs data from multiple tables can just get it
+
+    // when the app runs it would build that database a fill the table then when it runs
+    // again it checks if it's built
+
+    // same function to check database
+    // in mainActivity
+    // getallresources
+    // if FALSE
+    // then load
+    // else cont.
+
+
+    // can load an sql file, but can be harder, so better to have some functions that loads all the
+    // data into DB
+
+
+    // can start setting up how list of resources would look like and the view of the resource for now
+    // will be the same for each resource, the values would change with liveData
+    // can start building entity and data access object
+
 }
