9b9e6e00c4128e411ffc67fc52e3fe5457af420c
Thomas Fang <tfang124120@csu.fullerton.edu>
2022-04-06 20:03:07 -0700

    Update : Added skeleton for posting section

--- app/src/main/java/com/example/lico/MainActivity.kt
+++ app/src/main/java/com/example/lico/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.lico
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.lico.fragments.AddFragment
+import com.example.lico.fragments.DashboardFragment
+import com.example.lico.fragments.ViewFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val dashboardFragment = DashboardFragment()
+    private val addFragment = AddFragment()
+    private val viewFragment = ViewFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(dashboardFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.ic_dashboard -> replaceFragment(dashboardFragment)
+                R.id.ic_add -> replaceFragment(addFragment)
+                R.id.ic_view -> replaceFragment(viewFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
