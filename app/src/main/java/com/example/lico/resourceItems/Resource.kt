3fab381013d9fee300188ee91f7f5a1155480195
vtran <vtran2535@csu.fullerton.edu>
2022-05-05 12:58:48 -0700

    Inputted resourceArray into string.xml(essentially hardcoded) and fixed issue with button action from homepage

--- app/src/main/java/com/example/lico/resourceItems/Resource.kt
+++ app/src/main/java/com/example/lico/resourceItems/Resource.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.lico.MainActivity
 import com.example.lico.R
 import com.example.lico.database.LicoDatabase
 import com.example.lico.database.ResourceEntity
@@ -29,9 +32,16 @@ class Resource:Fragment() {
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val context = context as MainActivity
+        val centerList = resources.getStringArray(R.array.resourceArray)
+
+        val lv = context.findViewById(R.id.resourceListView) as ListView
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, centerList)
+        lv.adapter = adapter
     }
+
 
 }
